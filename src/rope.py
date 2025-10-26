590ccf2da46d9a19b13af18b7b9cb4d9932fc1b2
brrrrcad <burghardist@gmail.com>
2025-10-26 19:59:55 +0100

    StandingEnd implementation fixed

--- src/rope.py
+++ src/rope.py
@@ -6,27 +6,35 @@ class LocationBase():
     def __init__(self, rope):
         self.rope = rope
 
-    def get_location(self):
+    def _get_location(self):
         raise NotImplementedError()
 
 class WorkingEnd(LocationBase):
-    def get_location(self):
-        return (self.rope.get_largest_location() + 1) / 2
+    def _get_location(self):
+        return (self.rope._get_largest_location() + 1) / 2
+    
+class StandingEnd(LocationBase):
+    def _get_location(self):
+        return (self.rope._get_smallest_location()) / 2
 
 class Location(LocationBase):
     def __init__(self, rope, lacation:float):
         super().__init__(rope)
         self.lacation = lacation
 
-    def get_location(self):
+    def _get_location(self):
         return self.lacation
     
 
 class Rope():
     def __init__(self):
         self.w = WorkingEnd(self)
-        self.s = Location(self, 0)
-        self.locations = [self.s]
+        self.s = StandingEnd(self)
+        self.loc_placeholder = Location(self, 0.5)
+        self.locations = [self.loc_placeholder]
     
-    def get_largest_location(self):
-        return max([loc.get_location() for loc in self.locations])
+    def _get_largest_location(self):
+        return max([loc._get_location() for loc in self.locations])
+    
+    def _get_smallest_location(self):
+        return min([loc._get_location() for loc in self.locations])
